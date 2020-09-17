7cdd743cdcf23898e35eedbd9e3283a6991cd23e
heili007 <35800712+heili007@users.noreply.github.com>
2020-09-17 18:34:36 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'client_id' => 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa',
-  'client_secret' => 'EIVCx5ztMSxMsga18MQ7rmGf9EIP7zv6tfimb0Kp5Uc=',
+  'client_id' => 'a1c4930a-d34c-4f32-af87-06c48382ad38',
+  'client_secret' => '-~ktOPXVrw902vRO-Q~2KFcIs3oR0b994.',
   'redirect_uri' => 'https://ju.tn/onedrive-login',
   'dir_cache_time' => 600,
   'file_cache_time' => 86400,
